c8766dd4083534f678cfcf6ad29f6e9ccd144181
Leighton Smallshire <lsmallshire@innov8gaming.com>
2025-10-28 11:50:09 +0000

    dumb-bot-deployer

--- new/deployer.py
+++ new/deployer.py
@@ -9,7 +9,7 @@ assert __name__ == "__main__", 'Must be run directly'
 
 # --- Configuration ---
 GITHUB_SECRET = os.environ.get("GITHUB_SECRET")
-BOT_DIR = '/bot_dir'
+BOT_DIR = './bot_dir'
 
 log_file = open('deploy_log.txt', 'a')
 
@@ -20,6 +20,7 @@ bot_process: None | subprocess.Popen = None
 
 
 def restart():
+    global bot_process
     if bot_process is not None:
         bot_process.terminate()
         try:
@@ -30,9 +31,9 @@ def restart():
     #
     username = 'LeightonSmallshire'
     password = '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'
-    url = f'https://{username}:{password}@github.com/LeightonSmallshire/iter8-bot/archive/refs/heads/main.zip'
+    url = f'https://{username}:{password}@github.com/LeightonSmallshire/iter8-bot/archive/refs/heads/main-v2.zip'
 
-    subprocess.run(['rm', '-rf', BOT_DIR], cwd='/')
+    subprocess.run(['rm', '-rf', BOT_DIR], cwd='/', check=True)
 
     # Start the wget process and pipe its output to tar
     with subprocess.Popen(["wget", "-qO-", url], stdout=subprocess.PIPE, cwd=BOT_DIR) as wget_process:
@@ -65,5 +66,10 @@ def get_status():
         # "webhook_endpoint": f"http://{WEBHOOK_HOST}:{WEBHOOK_PORT}/webhook"
     }
 
+
+try:
+    restart()
+except BaseException as e:
+    print(repr(e), file=log_file)
 
 uvicorn.run(app, host='0.0.0.0', port=8090)
