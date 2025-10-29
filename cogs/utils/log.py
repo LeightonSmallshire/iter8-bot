4958e6dbc41383c231b7faa7badb8e49d7abf6bd
Nathan Mayes <nmayes@innov8gaming.com>
2025-10-29 16:14:27 +0000

    Make DB async + start shop

--- cogs/utils/log.py
+++ cogs/utils/log.py
@@ -1,10 +1,10 @@
-# from .database import write_log
+from .database import write_log
 import logging
+import asyncio
     
 class DatabaseHandler(logging.Handler):
     def emit(self, record: logging.LogRecord) -> None:
         try:
-            pass
-            #write_log(record.levelname, record.getMessage())
+            asyncio.create_task(write_log(record.levelname, record.getMessage()))
         except Exception:
             self.handleError(record)
