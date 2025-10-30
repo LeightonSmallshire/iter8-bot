ed67386f68022ff9bed17f7c085d34dddf6aa336
Leighton Smallshire <lsmallshire@innov8gaming.com>
2025-10-30 15:08:28 +0000

    download autocomplete and add zipping

---
+++ utils/files.py
@@ -0,0 +1,16 @@
+import io
+import os
+import zipfile
+
+
+def zip_directory(path: str):
+    zip_buffer = io.BytesIO()
+
+    with zipfile.ZipFile(zip_buffer, zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, start=path)
+                zipf.write(file_path, arcname)
+
+    return zip_buffer
