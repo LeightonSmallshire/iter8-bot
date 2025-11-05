e9672070e38636886567d664e884270e011c94a4
Nathan Mayes <nmayes@innov8gaming.com>
2025-11-05 11:04:48 +0000

    Improve admin roll

--- cogs/admin_roll_cog.py
+++ cogs/admin_roll_cog.py
@@ -4,6 +4,8 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import logging
+from zoneinfo import ZoneInfo
+from datetime import time, timedelta
 import random
 import asyncio
 import utils.bot as bot_utils
@@ -14,6 +16,24 @@ _log = logging.getLogger(__name__)
 _log.addHandler(logging.FileHandler('data/logs.log'))
 _log.addHandler(log_utils.DatabaseHandler())
 
+
+ROLL_GIF_URL = "https://media.tenor.com/XYkAxffY_PsAAAAM/dice-bae-dice.gif"
+
+def is_correct_time(interaction: discord.Interaction) -> bool:
+    UK = ZoneInfo("Europe/London")
+    dt_utc = interaction.created_at
+    dt_uk = dt_utc.astimezone(UK)
+
+    return dt_uk.weekday() == 4 and dt_uk.time() >= time(13, 0)
+
+async def is_first_roll(interaction: discord.Interaction) -> bool:
+    UK = ZoneInfo("Europe/London")
+    dt_utc = interaction.created_at
+
+    roll_info = await db_utils.get_last_admin_roll()
+    time_passed = (dt_utc - roll_info.last_roll) > timedelta(days=6) if roll_info else True
+
+    return time_passed
 
 class AdminRollCog(commands.Cog):
     def __init__(self, client: discord.Client):
@@ -26,12 +46,16 @@ class AdminRollCog(commands.Cog):
     @app_commands.command(name='roll_admin', description='Commence the weekly admin dice roll.')
     @commands.check(bot_utils.is_guild_paradise)
     async def command_roll_admin(self, interaction: discord.Interaction):
-        if not await bot_utils.is_user_role(interaction, bot_utils.Roles.DiceRoller):
-            await interaction.response.send_message(f"Only <@&{bot_utils.Roles.DiceRoller}> may roll the admin.")
+        if not is_correct_time(interaction):
+            await interaction.response.send_message(f"Wait till you've had your samosa!")
             return
 
+        if not await is_first_roll(interaction):
+            await interaction.response.send_message(f"The dice has already been rolled, respect its result (unless you have a reroll token).")
+            return
+        
         await self.do_admin_roll(interaction, False)
-
+        await db_utils.update_last_admin_roll()
 
     @app_commands.command(name='reroll', description='Use a purchased reroll token to re-roll the admin.')
     @commands.check(bot_utils.is_guild_paradise)
@@ -60,24 +84,30 @@ class AdminRollCog(commands.Cog):
         def make_emoji_number(num: int):
             return "".join([f":number_{d}:" for d in str(num)])
 
-        embed = discord.Embed(title=title, color=discord.Color.yellow())
+        list_embed = discord.Embed(title=title, color=discord.Color.yellow())
         for idx, user_id in enumerate(roll_table, 1):
-            embed.add_field(
+            list_embed.add_field(
                 name=make_emoji_number(idx),
                 value=f"<@{user_id}>",
                 inline=False,
             )
 
-        await interaction.followup.send(embed=embed)
-        msg = await interaction.followup.send("Rolling...", wait=True)
-
-        # Sleep for dramatic effect
+        await interaction.followup.send(embed=list_embed)
+        
         await asyncio.sleep(3)
 
-        index = random.randrange(0, len(roll_table))
-        await msg.edit(content=f"A :number_{make_emoji_number(index)}: was rolled!")
+        roll_embed = discord.Embed(title="Rolling...")
+        roll_embed.set_image(url=ROLL_GIF_URL)
 
-        await asyncio.sleep(1)
+        msg = await interaction.followup.send(embed=roll_embed, wait=True)
+
+        # Sleep for dramatic effect
+        await asyncio.sleep(4)
+
+        index = random.randrange(0, len(roll_table))
+        await msg.edit(content=f"A {make_emoji_number(index + 1)} was rolled!", embed=None)
+
+        await asyncio.sleep(3)
 
         choice = roll_table[index]
         new_admin = await interaction.guild.fetch_member(choice)
